ad7cbb3e56ab6e7f221612a53f61e96a180f1261
dangvanmih <minhmeo2k413@gmail.com>
2026-08-21 14:47:57 +0700

    env config

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 //Import 
 const express = require('express');
+require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 3000;
 const router = require('./routers/client/index.router')
+const port = process.env.PORT;
 
 //Configue pug
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-
 //route
 router(app);
 
 //Start the server and listen on the defined port
-app.listen(PORT, () => {
-  console.log(`Server is successfully running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is successfully running on http://localhost:${port}`);
 });
