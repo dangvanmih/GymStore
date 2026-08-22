6d5647204d530beb51c54adaf269e190fc972064
dangvanmih <minhmeo2k413@gmail.com>
2026-08-22 15:05:56 +0700

    sửa lại layout

--- public/client/js/script.js
+++ public/client/js/script.js
@@ -36,3 +36,42 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 // end searchOverlay
+
+
+//scroll web
+document.addEventListener("DOMContentLoaded", function () {
+  const lenis = new Lenis({
+    duration: 0.17,      // Thời gian mượt (giảm số này thì nhanh hơn, tăng thì chậm hơn)
+    easing: (t) => t,   // Hàm easing, có thể custom
+    smooth: true,
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+});
+//end-scroll-web
+
+// scroll-header
+let lastScroll = 0; //vị trí scroll của lần trước
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.scrollY;
+
+  if (currentScroll > lastScroll) { //đang scroll xuống
+    // scroll xuống
+    header.classList.add("hide");
+
+  } else { //đang scroll lên
+    // scroll lên
+    header.classList.remove("hide");
+  }
+
+  lastScroll = currentScroll; // sau mỗi lần scroll thì gán lại vị trí scroll mới nhất
+});
+
+//end-scrol-header
