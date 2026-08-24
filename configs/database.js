2f4103f8cc3861f650ca8e85395144db11c89c40
dangvanmih <minhmeo2k413@gmail.com>
2026-08-24 11:10:38 +0700

    làm trang sản phẩm bên client

--- configs/database.js
+++ configs/database.js
@@ -4,7 +4,7 @@ module.exports.connect = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
     console.log("Connect Success!");
-    
+
   } catch (error) {
     console.log("Connect Error!");
     
--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,18 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+
+  const products = await Product.find({
+    status: "active",
+    deleted: false
+  });
+
+  const newProducts = products.map(item => {
+    item.priceNew = item.price - (item.price * item.discountPercentage / 100);
+  })
+
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
-  })
+    products: newProducts
+  });
 }
