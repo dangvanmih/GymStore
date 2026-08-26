6bca198b4a5e020d73b6c745ac01d1f1d13a8fe2
dangvanmih <minhmeo2k413@gmail.com>
2026-08-26 22:48:18 +0700

    Tạo router và cấu trúc MVC bên admin

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const database = require('./configs/database.js');
 require('dotenv').config();
 
-const router = require('./routers/client/index.router')
-
+const routerClient = require('./routers/client/index.router')
+const routerAdmin = require('./routers/admin/index.router')
 database.connect();
 const app = express();
 const port = process.env.PORT;
@@ -16,7 +16,8 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 
 //route
-router(app);
+routerClient(app);
+routerAdmin(app);
 
 //Start the server and listen on the defined port
 app.listen(port, () => {
