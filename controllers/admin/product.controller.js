50710996a60747daf3829f1470fb53155d74c65a
dangvanmih <minhmeo2k413@gmail.com>
2026-09-15 21:59:48 +0700

    tối ưu tính năng search - tách mixin search (FE) - tách search helper (BE)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,7 @@
 const Product = require('../../models/product.model');
 const filterStatusHelper = require('../../helpers/filterStatus');
+const searchHelper = require('../../helpers/search');
+
 // GET: /admin/product
 module.exports.index = async (req, res) => {
 
@@ -16,12 +18,10 @@ module.exports.index = async (req, res) => {
   };
 
   // xử lý tìm kiếm sản phẩm theo từ khóa
-  let keyword = "";
-  if (req.query.keyword) {
-    keyword = req.query.keyword;
-    const regex = new RegExp(keyword, 'i'); // 'i' để tìm kiếm không phân biệt chữ hoa chữ thường
-    findQuery.title = regex;
-  };
+  const objectSearch = searchHelper(req);
+  if (objectSearch.regex) {
+    findQuery.title = objectSearch.regex;
+  }
 
   const products = await Product.find(findQuery)
 
@@ -30,6 +30,6 @@ module.exports.index = async (req, res) => {
     pageTitle: 'Trang sản phẩm',
     products: products,
     filterStatus: filterStatus,
-    keyword: keyword
+    keyword: objectSearch.keyword
   });
 }
